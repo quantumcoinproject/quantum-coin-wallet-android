ea93548cea5b682a613ea166a3072233896d4cb1
wednesday110603 <125394169+wednesday110603@users.noreply.github.com>
2024-10-29 22:04:05 +0530

    Update install-android-library-ubuntu.sh

--- install-android-library-ubuntu.sh
+++ install-android-library-ubuntu.sh
@@ -27,25 +27,25 @@ sudo rm -r $PWD/app/src/main/jniLibs/armeabi-v7a/*
 sudo rm -r $PWD/app/src/main/jniLibs/x86/*
 sudo rm -r $PWD/app/src/main/jniLibs/x86_64/*
 
-curl -Lo $PWD/templibs/hybrid-pqc/includes.zip https://github.com/DogeProtocol/hybrid-pqc/releases/download/v0.1.39/includes.zip
+curl -Lo $PWD/templibs/hybrid-pqc/includes.zip https://github.com/quantumcoinproject/hybrid-pqc/releases/download/v0.1.42/includes.zip
 unzip $PWD/templibs/hybrid-pqc/includes.zip -d $PWD/templibs/hybrid-pqc
-echo "357BF7878A6910990DF69641815112BCD23CB53FE3DCEBF9930E617E00DBED1D $PWD/templibs/hybrid-pqc/includes.zip" | sha256sum --check  - || exit 1
+echo "E6DE4035B5DCA73970A73F802CCC25894B59056EB48533CCCED530A7ECC46DEC $PWD/templibs/hybrid-pqc/includes.zip" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/hybrid-pqc/android-arm64-v8a.tar.gz https://github.com/DogeProtocol/hybrid-pqc/releases/download/v0.1.39/android-arm64-v8a.tar.gz
+curl -Lo $PWD/templibs/hybrid-pqc/android-arm64-v8a.tar.gz https://github.com/quantumcoinproject/hybrid-pqc/releases/download/v0.1.42/android-arm64-v8a.tar.gz
 tar -zxf $PWD/templibs/hybrid-pqc/android-arm64-v8a.tar.gz --directory $PWD/templibs/hybrid-pqc//android-arm64-v8a
-echo "e0d5d67850be55a4304f83157c1ecac852994a6afd417d2729f524b1909653e1 $PWD/templibs/hybrid-pqc/android-arm64-v8a.tar.gz" | sha256sum --check  - || exit 1
+echo "5fd4926f1b7c3f2f4519860fee38c43de65f47d31aaab7701853e29b535a9570 $PWD/templibs/hybrid-pqc/android-arm64-v8a.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/hybrid-pqc/android-armeabi-v7a.tar.gz https://github.com/DogeProtocol/hybrid-pqc/releases/download/v0.1.39/android-armeabi-v7a.tar.gz
+curl -Lo $PWD/templibs/hybrid-pqc/android-armeabi-v7a.tar.gz https://github.com/quantumcoinproject/hybrid-pqc/releases/download/v0.1.42/android-armeabi-v7a.tar.gz
 tar -zxf $PWD/templibs/hybrid-pqc/android-armeabi-v7a.tar.gz --directory $PWD/templibs/hybrid-pqc/android-armeabi-v7a
-echo "58e78dfa86b897c43bfafce53893e6a9ccab0184bedf1cf5fd21bdd6dcc9dc87 $PWD/templibs/hybrid-pqc/android-armeabi-v7a.tar.gz" | sha256sum --check  - || exit 1
+echo "7005851c18db74e1aafc806f2d28635fb9839b349ebd61ea50f4904cf6c9eae7 $PWD/templibs/hybrid-pqc/android-armeabi-v7a.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/hybrid-pqc/android-x86.tar.gz https://github.com/DogeProtocol/hybrid-pqc/releases/download/v0.1.39/android-x86.tar.gz
+curl -Lo $PWD/templibs/hybrid-pqc/android-x86.tar.gz https://github.com/quantumcoinproject/hybrid-pqc/releases/download/v0.1.42/android-x86.tar.gz
 tar -zxf $PWD/templibs/hybrid-pqc/android-x86.tar.gz --directory $PWD/templibs/hybrid-pqc/android-x86
-echo "528d3a3b046b5312687fb56a31031f0df13c95a9e6c6f7e8b85a8c30262c21af $PWD/templibs/hybrid-pqc/android-x86.tar.gz" | sha256sum --check  - || exit 1
+echo "f1268751de443aaf656012a68fea8e000944db0c99863f4bf33c126cdce8a5cc $PWD/templibs/hybrid-pqc/android-x86.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/hybrid-pqc/android-x86_64.tar.gz https://github.com/DogeProtocol/hybrid-pqc/releases/download/v0.1.39/android-x86_64.tar.gz
+curl -Lo $PWD/templibs/hybrid-pqc/android-x86_64.tar.gz https://github.com/quantumcoinproject/hybrid-pqc/releases/download/v0.1.42/android-x86_64.tar.gz
 tar -zxf $PWD/templibs/hybrid-pqc/android-x86_64.tar.gz --directory $PWD/templibs/hybrid-pqc/android-x86_64
-echo "5335a9e476ae67cf90657a83e6c8e8daadfd0553cf834a7151671cc6c1dc94fa $PWD/templibs/hybrid-pqc/android-x86_64.tar.gz" | sha256sum --check  - || exit 1
+echo "05a6d3b76cb22dc173a35d730568be047437062b00d9bc7fc8b29531b1865b37 $PWD/templibs/hybrid-pqc/android-x86_64.tar.gz" | sha256sum --check  - || exit 1
 
 sudo cp $PWD/templibs/hybrid-pqc/build/include/dilithium/hybrid.h $PWD/app/src/main/jniLibs/arm64-v8a/hybrid.h
 sudo cp $PWD/templibs/hybrid-pqc/build/include/dilithium/hybrid.h $PWD/app/src/main/jniLibs/armeabi-v7a/hybrid.h
@@ -72,21 +72,21 @@ sudo cp $PWD/templibs/hybrid-pqc/android-armeabi-v7a/libhybridpqc.so $PWD/app/sr
 sudo cp $PWD/templibs/hybrid-pqc/android-x86/libhybridpqc.so $PWD/app/src/main/jniLibs/x86/libhybridpqc.so
 sudo cp $PWD/templibs/hybrid-pqc/android-x86_64/libhybridpqc.so $PWD/app/src/main/jniLibs/x86_64/libhybridpqc.so
 
-curl -Lo $PWD/templibs/mobile-go/android-arm64-v8a.tar.gz https://github.com/DogeProtocol/go-dp/releases/download/v2.0.49/android-arm64-v8a.tar.gz
+curl -Lo $PWD/templibs/mobile-go/android-arm64-v8a.tar.gz https://github.com/quantumcoinproject/quantum-coin-go/releases/download/v2.0.51/android-arm64-v8a.tar.gz
 tar -zxf $PWD/templibs/mobile-go/android-arm64-v8a.tar.gz --directory $PWD/templibs/mobile-go/android-arm64-v8a
-echo "b03a6e856229b728652fd1af486875ac54081b8b4c546ef52aa093f0eb3dc0bd $PWD/templibs/mobile-go/android-arm64-v8a.tar.gz" | sha256sum --check  - || exit 1
+echo "0f17b2e2168a4e70f5f4dbe7388052bef4099b4b2dfe7ebe1670308e24be2173 $PWD/templibs/mobile-go/android-arm64-v8a.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/mobile-go/android-armeabi-v7a.tar.gz https://github.com/DogeProtocol/go-dp/releases/download/v2.0.49/android-armeabi-v7a.tar.gz
+curl -Lo $PWD/templibs/mobile-go/android-armeabi-v7a.tar.gz https://github.com/quantumcoinproject/quantum-coin-go/releases/download/v2.0.51/android-armeabi-v7a.tar.gz
 tar -zxf $PWD/templibs/mobile-go/android-armeabi-v7a.tar.gz --directory $PWD/templibs/mobile-go/android-armeabi-v7a
-echo "430c130d870ea83c7f8ac082b02ceb62ddf8a6d8be669f12baed62fca7645c04 $PWD/templibs/mobile-go/android-armeabi-v7a.tar.gz" | sha256sum --check  - || exit 1
+echo "0f17b2e2168a4e70f5f4dbe7388052bef4099b4b2dfe7ebe1670308e24be2173 $PWD/templibs/mobile-go/android-armeabi-v7a.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/mobile-go/android-x86.tar.gz https://github.com/DogeProtocol/go-dp/releases/download/v2.0.49/android-x86.tar.gz
+curl -Lo $PWD/templibs/mobile-go/android-x86.tar.gz https://github.com/quantumcoinproject/quantum-coin-go/releases/download/v2.0.51/android-x86.tar.gz
 tar -zxf $PWD/templibs/mobile-go/android-x86.tar.gz --directory $PWD/templibs/mobile-go/android-x86
-echo "ca86766e07bb487468c7c16833e45de6909a6dcd1edd6e4e3a9ca61709b901ce $PWD/templibs/mobile-go/android-x86.tar.gz" | sha256sum --check  - || exit 1
+echo "8d2525ff1f0aa53ceb42991c195cc732b99d94a00c07147509594164010cb15c $PWD/templibs/mobile-go/android-x86.tar.gz" | sha256sum --check  - || exit 1
 
-curl -Lo $PWD/templibs/mobile-go/android-x86_64.tar.gz https://github.com/DogeProtocol/go-dp/releases/download/v2.0.49/android-x86_64.tar.gz
+curl -Lo $PWD/templibs/mobile-go/android-x86_64.tar.gz https://github.com/quantumcoinproject/quantum-coin-go/releases/download/v2.0.51/android-x86_64.tar.gz
 tar -zxf $PWD/templibs/mobile-go/android-x86_64.tar.gz --directory $PWD/templibs/mobile-go/android-x86_64
-echo "22d676b41a725032dce6bf2a4bfd4fcafbcbda84587dd035425b61c6f843593e $PWD/templibs/mobile-go/android-x86_64.tar.gz" | sha256sum --check  - || exit 1
+echo "a7c5a37c0f23fd967e4095104ad6e8275a89d7dff797104a948d5f8ac822faef $PWD/templibs/mobile-go/android-x86_64.tar.gz" | sha256sum --check  - || exit 1
 
 sudo cp $PWD/templibs/mobile-go/android-arm64-v8a/* $PWD/app/src/main/jniLibs/arm64-v8a/
 sudo cp $PWD/templibs/mobile-go/android-armeabi-v7a/* $PWD/app/src/main/jniLibs/armeabi-v7a/
